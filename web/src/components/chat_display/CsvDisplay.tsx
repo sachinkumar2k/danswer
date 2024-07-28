9149c92344d652d140e3ebd03c08bd41a1112adc
pablodanswer <pablo@danswer.ai>
2024-07-27 19:52:02 -0700

    finalize csv display

--- web/src/components/chat_display/CsvDisplay.tsx
+++ web/src/components/chat_display/CsvDisplay.tsx
@@ -19,7 +19,7 @@ export default function CsvPage({ csvFileDescriptor, close }: { csvFileDescripto
   return (
     <>
       {expanded ?
-        <Modal onOutsideClick={() => setExpanded(false)} className='animate-all ease-in !p-0'>
+        <Modal onOutsideClick={() => setExpanded(false)} className='!max-w-5xl rounded-2xl animate-all ease-in !p-0'>
           <CsvSection close={close} expanded={expanded} expand={expand} csvFileDescriptor={csvFileDescriptor} />
         </Modal>
         :
@@ -86,10 +86,10 @@ export const CsvSection = ({ expand, csvFileDescriptor, expanded, close }: { clo
       document.body.removeChild(link);
     }
   }
-  
+
   return (
-    <div className={`${!expanded ? "!rounded !rounded-xl max-w-message-max" : "w-full"} !bg-neutral-50 w-full border `}>
-      <CardHeader className="w-full !py-0 !pb-4 border-b  border-b-neutral-200 !pt-4 !mb-0 z-[10] top-0">
+    <div className={`${!expanded ? "max-w-message-max" : "w-full"} !rounded !rounded-lg w-full border`}>
+      <CardHeader className="w-full !py-0 !pb-4 border-b border-b-neutral-200 !pt-4 !mb-0 z-[10] top-0">
         <div className='flex justify-between items-center'>
           <CardTitle className='!my-auto !text-xl'>
             {csvFileDescriptor.name}
@@ -98,17 +98,17 @@ export const CsvSection = ({ expand, csvFileDescriptor, expanded, close }: { clo
             <TooltipGroup>
               <CustomTooltip showTick line position='top' content="Download file">
                 <button onClick={() => downloadFile()}>
-                  <DownloadCSVIcon className='cursor-pointer transition-colors duration-300 hover:text-neutral-800 h-6 w-6 text-neutral-400' /> 
+                  <DownloadCSVIcon className='cursor-pointer transition-colors duration-300 hover:text-neutral-800 h-6 w-6 text-neutral-400' />
                 </button>
-              </CustomTooltip> 
-              <CustomTooltip line position='top' content={expanded ? "Minimize" : "Full screen"}> 
+              </CustomTooltip>
+              <CustomTooltip line position='top' content={expanded ? "Minimize" : "Full screen"}>
                 <button onClick={() => expand()}>
-                  {!expanded ? 
-                    <ExpandTwoIcon className='transition-colors duration-300 ml-4 hover:text-neutral-800 h-6 w-6 cursor-pointer text-neutral-400' /> 
+                  {!expanded ?
+                    <ExpandTwoIcon className='transition-colors duration-300 ml-4 hover:text-neutral-800 h-6 w-6 cursor-pointer text-neutral-400' />
                     :
-                    <DexpandTwoIcon className='transition-colors duration-300 ml-4 hover:text-neutral-800 h-6 w-6 cursor-pointer text-neutral-400' /> 
+                    <DexpandTwoIcon className='transition-colors duration-300 ml-4 hover:text-neutral-800 h-6 w-6 cursor-pointer text-neutral-400' />
                   }
-                </button> 
+                </button>
               </CustomTooltip>
               <CustomTooltip line position='top' content="No vis">
                 <button onClick={() => close()}>
@@ -120,12 +120,12 @@ export const CsvSection = ({ expand, csvFileDescriptor, expanded, close }: { clo
         </div>
       </CardHeader>
       <Card className="w-full max-h-[600px] !p-0 relative overflow-x-scroll overflow-y-scroll mx-auto">
-        <CardContent className='!mt-0 !p-0'>
+        <CardContent className='!p-0'>
           <Table>
-            <TableHeader>
+            <TableHeader className='!sticky !top-0 '>
               <TableRow className='!bg-neutral-100'>
                 {headers.map((header, index) => (
-                  <TableHead key={index}>{index == 0 ? "" : header}</TableHead>
+                  <TableHead className=' !sticky !top-0 ' key={index}>{index == 0 ? "" : header}</TableHead>
                 ))}
               </TableRow>
             </TableHeader>
@@ -134,7 +134,7 @@ export const CsvSection = ({ expand, csvFileDescriptor, expanded, close }: { clo
               {data.map((row, rowIndex) => (
                 <TableRow key={rowIndex}>
                   {headers.map((header, cellIndex) => (
-                    <TableCell className={`${cellIndex == 0 && "!bg-neutral-100"}`} key={cellIndex}>{row[header]}</TableCell>
+                    <TableCell className={`${cellIndex == 0 && "sticky left-0 !bg-neutral-100"}`} key={cellIndex}>{row[header]}</TableCell>
                   ))}
                 </TableRow>
               ))}
