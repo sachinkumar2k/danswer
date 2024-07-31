0729397c2aa8f259e570bc72b0ec160b2369c8f5
pablodanswer <pablo@danswer.ai>
2024-07-30 20:09:03 -0700

    minor

---
+++ web/src/app/admin/indexing/status/layout.tsx
@@ -0,0 +1,20 @@
+// import "./globals.css";
+
+// import { getCombinedSettings } from "@/components/settings/lib";
+// import { CUSTOM_ANALYTICS_ENABLED } from "@/lib/constants";
+// import { SettingsProvider } from "@/components/settings/SettingsProvider";
+// import { Metadata } from "next";
+// import { buildClientUrl } from "@/lib/utilsSS";
+// import { Inter } from "next/font/google";
+
+// export default async function RootLayout({
+//     children,
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     const combinedSettings = await getCombinedSettings({});
+
+//     return (
+//         { children }
+//     );
+// }
